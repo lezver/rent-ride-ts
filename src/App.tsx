5963b00af688861115445780c0e8b8d98e3a946b
Vladislav <lezver1994@gmail.com>
2023-12-25 18:43:16 +0200

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { NavLink, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Suspense } from 'react';
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+            </ul>
+            <Suspense>
+              <Outlet />
+            </Suspense>
+          </>
+        }
+      >
+        <Route index element={<section>Home</section>} />
+        <Route path="about" element={<div>About</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
